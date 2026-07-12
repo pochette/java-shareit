7799bb0cd155a2b094f3b94991c1c023d4a52017
Andrey Burdak <burdak1994@gmail.com>
2026-07-12 13:15:19 +0300

    fix: убрать warning у предыдущего комита

--- src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -17,7 +17,6 @@ import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
